54f92121dd3fb28cbc06e09f384a7e8a97740149
Ethan Yin <ethan.yin@jetbrains.com>
2024-12-15 13:26:27 +0800

    Initialize project with Gradle and Spring Boot setup
    Add necessary files and configurations to set up a new Spring Boot project using Gradle, including build scripts, application properties, and test classes. Configure the `.gitignore` and `.gitattributes` for proper version control.

---
+++ src/main/java/f/j/d/demo_in_tc/DemoInTcApplication.java
@@ -0,0 +1,13 @@
+package f.j.d.demo_in_tc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoInTcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoInTcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/f/j/d/demo_in_tc/DemoInTcApplicationTests.java
@@ -0,0 +1,13 @@
+package f.j.d.demo_in_tc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoInTcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
